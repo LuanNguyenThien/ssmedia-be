21fc6dbd8fd117734073be62b43bee20bc608c60
LuanNguyen <thienluan678@gmail.com>
2025-04-16 01:41:55 +0700

    setup socket-be for peer connection

--- src/shared/sockets/chat.ts
+++ src/shared/sockets/chat.ts
@@ -21,6 +21,53 @@ export class SocketIOChatHandler {
         socket.join(roomId);
         // socket.join(receiverSocketId);
       });
+
+      socket.on('call-user', (data: { userToCall: string, signal: any, callType: string, callerName: string }) => {
+        const { userToCall, signal, callType, callerName } = data;
+        console.log(data);
+        const senderSocketId: string = connectedUsersMap.get(socket.id) as string;
+        const receiverSocketId: string = connectedUsersMap.get(userToCall) as string;
+        if (!receiverSocketId) {
+          console.log(`Receiver with ID ${userToCall} is not connected.`);
+          socket.emit('call-rejected', { message: 'User is offline' });
+          return;
+        }
+        //Kiểm tra trạng thái người nhận cuộc gọi
+        //Đặt trạng thái cuộc gọi cho người nhận
+        this.io.to(receiverSocketId).emit('call-incoming', { signal, from: senderSocketId, callType, callerName });
+      });
+
+      socket.on('call-accepted', (data: { signal: any, to: string, callType: string }) => {
+        const { signal, to, callType } = data;
+        console.log(data);
+        const senderSocketId: string = connectedUsersMap.get(socket.id) as string;
+        const receiverSocketId: string = connectedUsersMap.get(to) as string;
+        this.io.to(receiverSocketId).emit('call-accepted', { signal, callType, from: senderSocketId });
+      });
+
+      socket.on('call-rejected', (data: { to: string }) => {
+        const { to } = data;
+        console.log(data);
+        const senderSocketId: string = connectedUsersMap.get(socket.id) as string;
+        const receiverSocketId: string = connectedUsersMap.get(to) as string;
+        this.io.to(receiverSocketId).emit('call-rejected', { from: senderSocketId });
+      });
+
+      socket.on('call-ended', (data: { to: string }) => {
+        const { to } = data;
+        console.log(data);
+        const senderSocketId: string = connectedUsersMap.get(socket.id) as string;
+        const receiverSocketId: string = connectedUsersMap.get(to) as string;
+        if(!receiverSocketId) {
+          return;
+        }
+        console.log(`Receiver socket ID: ${receiverSocketId}`);
+        this.io.to(receiverSocketId).emit('call-ended', { from: senderSocketId });
+      });
+
+      socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`);
+      });
     });
   }
 }
