0974447d024a3dd744fc14996cd2db63d6ae9de6
LuanNguyen <thienluan678@gmail.com>
2025-06-03 18:01:27 +0700

    fix error while build check lib, mock.test failt

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-NODE_ENV=production pm2 start ./build/src/app.js -i 3 --no-daemon | ./node_modules/.bin/bunyan
+NODE_ENV=production pm2 start ./build/src/app.js -i 2 --no-daemon | ./node_modules/.bin/bunyan
